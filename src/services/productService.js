a04e219cb39c971852a9699f281fe70ea66d15e5
Leticia Kawamoto <leticiac.0401@gmail.com>
2023-07-13 15:40:50 -0300

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,11 +1,12 @@
 const { insertQuery, selectQuery } = require('../config/configDB');
 
 const getProducts = async() => {
-    // TODO: Implement method
+    await selectQuery('SELECT * FROM Products')
 };
 
 const addProduct = async(product) => {
-    // TODO: Implement method
+    await insertQuery(`INSERT INTO Products (description, full_description, category, price, restaurant_id, active)
+     VALUES ('${product.description}','${product.full_description}','${product.category}',${product.price},${product.restaurant_id},${product.active})`);
 };
 
 module.exports = {
